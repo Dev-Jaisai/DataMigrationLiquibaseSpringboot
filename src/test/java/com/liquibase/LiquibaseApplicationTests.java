16090ee99b09b80dbbd245de346974913aac436d
jaisaisarkate <jaisaisarkates0505@gmail.com>
2024-11-05 10:12:09 +0530

    integrate Liquibase for database migrations in Spring Boot project

---
+++ src/test/java/com/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+	@Test
+	void contextLoads() {
+		//No USe
+	}
+
+
+}
